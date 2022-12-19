66485aef7e1964b5a839b5406e1f5f3a34062b6a
tanmayvaij <tanmayvaij22@gmail.com>
2022-12-19 19:44:31 +0530

    navbar corrected

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,9 @@ import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
     return (
-        <div id="navbar" className="fixed text-white p-2">
+        <div id="navbar" className="flex justify-center">
             
-            <ul className="flex flex-wrap text-xs">
+            <ul className="fixed flex flex-wrap text-xs text-white p-2 rounded-xl">
                 <span></span>
                 <NavLink className="hover:text-pink-500 p-2" to="/">Login</NavLink>
                 <NavLink className="hover:text-pink-500 p-2" to="/signup">Registration</NavLink>
